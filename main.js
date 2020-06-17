f4301ea3d1b07269924a5477307267837d55c8ee
Ruben Rodriguez <ruben.rodriguez@codeimmersives.com>
2020-06-17 14:15:33 -0400

    I think app works now

--- main.js
+++ main.js
@@ -1,4 +1,42 @@
 // Your code here!
+let colorCombinator = require("./color-combinator")
+let colorDeconstructor = require("./color-deconstructor")
+
+let color1 = getInput(0);
+let color2 = getInput(1);
+
+
+
+if(color1 !== 'red' || color1 !== 'purple' || color1 !== 'green'){
+  console.log("Sorry, that's not a secondary color!")
+}
+
+if(color1 === undefined && color2 === undefined){
+  console.log("Please enter a color.")
+} 
+if(color1 === 'orange' || color1 === 'purple' || color1 === 'green'  && color2 === undefined){
+   console.log(colorDeconstructor(color1))
+}
+if(color1 === 'red' && color2 === 'yellow'){
+  console.log(colorCombinator(color1, color2))
+}
+if(color1 === 'red' && color2 === 'blue'){
+  console.log(colorCombinator(color1, color2))
+}
+if(color1 === 'yellow' && color2 === 'blue'){
+  console.log(colorCombinator(color1, color2))
+}
+if(color1 === 'yellow' && color2 === 'red'){
+  console.log(colorCombinator(color1, color2))
+}
+if(color1 === 'blue' && color2 === 'red'){
+  console.log(colorCombinator(color1, color2))
+}
+if(color1 === 'blue' && color2 === 'yellow'){
+  console.log(colorCombinator(color1, color2))
+}
+
+
 
 
 
